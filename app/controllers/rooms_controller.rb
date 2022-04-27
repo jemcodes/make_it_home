1b5311ca428deb772dd4163a1e972b534c736203
jane martin <martin.jane.ellis@gmail.com>
2022-04-27 18:50:37 -0400

    Add ability to edit rooms and refactor form views to use partial

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -21,10 +21,22 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+
+    if @room.update(room_params)
+      redirect_to @room
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def room_params
       params.require(:room).permit(:title, :room_image)
     end
-
-
 end
