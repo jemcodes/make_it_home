9846e1562a0a0427c305f135d1d1f44e5a6a744e
jane martin <martin.jane.ellis@gmail.com>
2022-05-15 17:36:58 -0400

    Set up basic delete ability

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,13 @@ class TasksController < ApplicationController
     redirect_to room_path(@room)
   end
 
+  def destroy
+    @room = Room.find(params[:room_id])
+    @task = room.tasks.find(params[:id])
+    @task.destroy
+    redirect_to room_path(@room), status: 303
+  end
+
   private
     def task_params
       params.require(:task).permit(:body, :status)
